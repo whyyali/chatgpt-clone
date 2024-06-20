dc1f81826591dafedf82f59b111fcc35f3a3fdc1
aliraza <chaudaryaliraza0@gmail.com>
2024-06-20 12:57:47 +0500

    add login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,75 @@
+import { useFonts } from 'expo-font';
+import { Slot, SplashScreen, Stack, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import * as SecureStore from 'expo-secure-store';
 import { Ionicons } from '@expo/vector-icons';
-import { Stack, router } from 'expo-router';
 import { TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function initialLayout() {
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+// Cache the Clerk JWT
+const tokenCache = {
+    async getToken(key: string) {
+        try {
+            return SecureStore.getItemAsync(key);
+        } catch (err) {
+            return null;
+        }
+    },
+    async saveToken(key: string, value: string) {
+        try {
+            return SecureStore.setItemAsync(key, value);
+        } catch (err) {
+            return;
+        }
+    },
+};
+
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+    const [loaded, error] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+    const { isLoaded, isSignedIn } = useAuth();
+    const segments = useSegments();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (error) throw error;
+    }, [error]);
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    useEffect(() => {
+        if (!isLoaded) return;
+
+        const inAuthGroup = segments[0] === '(auth)';
+
+        if (isSignedIn && !inAuthGroup) {
+            router.replace("(auth)")
+        } else if (!isSignedIn) {
+            router.replace('/');
+        }
+    }, [isSignedIn]);
+
+    if (!loaded || !isLoaded) {
+        return <Slot />;
+    }
+
     return (
         <Stack>
-            <Stack.Screen name='index' options={{ headerShown: false }} />
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                }}
+            />
             <Stack.Screen
                 name="login"
                 options={{
@@ -21,6 +85,19 @@ export default function initialLayout() {
                     ),
                 }}
             />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
-    )
-}
+    );
+};
+
+const RootLayoutNav = () => {
+    return (
+        <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache}>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <InitialLayout />
+            </GestureHandlerRootView>
+        </ClerkProvider>
+    );
+};
+
+export default RootLayoutNav;
--- app/login.tsx
+++ app/login.tsx
@@ -1,89 +1,136 @@
 import Colors from '@/constants/Colors';
 import { defaultStyles } from '@/constants/Styles';
+import { useSignIn, useSignUp } from '@clerk/clerk-expo';
 import { useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
-import { ActivityIndicator, Image, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput, Text, Alert, ActivityIndicator, TouchableOpacity, KeyboardAvoidingView, Image, Platform, } from 'react-native';
 
-const login = () => {
-    const {type} = useLocalSearchParams<{type: string}>();
+const Login = () => {
+    const { type } = useLocalSearchParams<{ type: string }>();
+    const { signIn, setActive, isLoaded } = useSignIn();
+    const { signUp, isLoaded: signUpLoaded, setActive: signupSetActive } = useSignUp();
 
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
 
-    const onSignInPress = async () =>{
+    const onSignInPress = async () => {
+        if (!isLoaded) {
+            return;
+        }
+        setLoading(true);
+        try {
+            const completeSignIn = await signIn.create({
+                identifier: emailAddress,
+                password,
+            });
 
-    }
+            await setActive({ session: completeSignIn.createdSessionId });
+        } catch (err: any) {
+            Alert.alert(err.errors[0].message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    const onSignUpPress = async () =>{
-        
-    }
+    const onSignUpPress = async () => {
+        if (!signUpLoaded) {
+            return;
+        }
+        setLoading(true);
 
-  return (
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} keyboardVerticalOffset={70} style={styles.container}>
-        {loading && (
-            <View style={defaultStyles.loadingOverlay}>
-                <ActivityIndicator size={"large"} color={"#fff"} />
+        try {
+            const result = await signUp.create({
+                emailAddress,
+                password,
+            });
+
+            signupSetActive({ session: result.createdSessionId });
+        } catch (err: any) {
+            alert(err.errors[0].message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={70}
+            style={styles.container}>
+            {loading && (
+                <View style={defaultStyles.loadingOverlay}>
+                    <ActivityIndicator size="large" color="#fff" />
+                </View>
+            )}
+
+            <Image source={require('../assets/images/logo-dark.png')} style={styles.logo} />
+
+            <Text style={styles.title}>{type === 'login' ? 'Welcome back' : 'Create your account'}</Text>
+            <View style={{ marginBottom: 30 }}>
+                <TextInput
+                    autoCapitalize="none"
+                    placeholder="john@apple.com"
+                    value={emailAddress}
+                    onChangeText={setEmailAddress}
+                    style={styles.inputField}
+                />
+                <TextInput
+                    placeholder="password"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                    style={styles.inputField}
+                />
             </View>
-        )}
 
-        <Image source={require('../assets/images/logo-dark.png')} style={styles.logo} />
-
-        <Text style={styles.title} >{type === 'login' ? 'Welcome back' : 'Create your account'}</Text>
-
-        <View style={{marginBottom: 30}}>
-            <TextInput autoCapitalize='none' placeholder='imaliraza10@gmail.com' value={emailAddress} onChangeText={setEmailAddress} style={styles.inputField} />
-            <TextInput placeholder='password' value={password} onChangeText={setPassword} style={styles.inputField} secureTextEntry />
-        </View>
-
-        {type === 'login' ? (
-            <TouchableOpacity style={[defaultStyles.btn, styles.btnPrimary]} onPress={onSignInPress}>
-                <Text style={styles.btnPrimaryText}>Login</Text>
-            </TouchableOpacity>
-        ) : (
-            <TouchableOpacity style={[defaultStyles.btn, styles.btnPrimary]} onPress={onSignUpPress}>
-                <Text style={styles.btnPrimaryText}>Create account</Text>
-            </TouchableOpacity>
-        )}
-    </KeyboardAvoidingView>
-  )
-}
-
+            {type === 'login' ? (
+                <TouchableOpacity style={[defaultStyles.btn, styles.btnPrimary]} onPress={onSignInPress}>
+                    <Text style={styles.btnPrimaryText}>Login</Text>
+                </TouchableOpacity>
+            ) : (
+                <TouchableOpacity style={[defaultStyles.btn, styles.btnPrimary]} onPress={onSignUpPress}>
+                    <Text style={styles.btnPrimaryText}>Create account</Text>
+                </TouchableOpacity>
+            )}
+        </KeyboardAvoidingView>
+    );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding: 20
+    container: {
+        flex: 1,
+        padding: 20,
     },
-    logo:{
+    logo: {
         width: 60,
         height: 60,
         alignSelf: 'center',
-        marginVertical: 80
+        marginVertical: 80,
     },
-    title:{
+    title: {
         fontSize: 30,
         marginBottom: 20,
         fontWeight: 'bold',
-        alignSelf: 'center'
+        alignSelf: 'center',
     },
-    inputField:{
+    inputField: {
         marginVertical: 4,
         height: 50,
         borderWidth: 1,
         borderColor: Colors.primary,
         borderRadius: 12,
         padding: 10,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
     },
-    btnPrimary:{
+    btnPrimary: {
         backgroundColor: Colors.primary,
         marginVertical: 4,
     },
-    btnPrimaryText:{
+    btnPrimaryText: {
         color: '#fff',
-        fontSize: 16
-    }
-})
+        fontSize: 16,
+    },
+});
 
-export default login
+export default Login;
